040dc80a46be71129637a9bde3c5f7e0d5b8915f
wkc75 <wongkinchong75@gmail.com>
2025-04-28 13:50:55 +0800

    Update ReverseWords.py

--- exercises/ReverseWords.py
+++ exercises/ReverseWords.py
@@ -1,12 +1,4 @@
-# Pseudo Code
-# Function reverse_words(sentence):
-#     Split sentence into list of words
-#     Initialize empty list reversed_words
-#     For each word in words:
-#         Reverse the characters in word
-#         Add reversed word to reversed_words
-#     Join reversed_words with spaces
-#     Return joined string
+
 
 def reverse_word(sentence):
     i = len(sentence)
